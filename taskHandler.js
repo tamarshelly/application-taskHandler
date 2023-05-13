9d652b83c92d0d23fdedb2c29913c24a73a4b534
tamarshelly <tamarshelly1@gmail.com>
2023-05-13 19:45:25 +0300

    adding arrow+delete functions

--- taskHandler.js
+++ taskHandler.js
@@ -16,18 +16,19 @@ function newElement() {
     document.getElementById("newTask").value = "";
     var cont =
       '<div class="cell"> <div class="side-scroll"> <ul class="arrow">' +
-      '<li><i class="fa fa-angle-up click" onclick="onClickUp(this)"></i></li> <li><i class="fa fa-angle-down click"></i></li>' +
+      '<li><i class="fa fa-angle-up click" onclick="onClickUp(this)"></i></li> <li><i class="fa fa-angle-down click" onclick="onClickDown(this)"></i></li>' +
       '</ul></div><div class="content"> <ul class="label-cell">  <li class="task-label id="label' +
       contador +
       '">' +
       inputValue +
-      '</li>  <li><i class="fa fa-edit click" style="font-size: 17px"></i></li> </ul> </div>' +
-      '<div class="features">  <i class="fa fa-trash-o click" style="font-size: 24px; display: table"' +
+      '</li>  <li><i class="fa fa-edit click" onClick="onClickEdit(this)" style="font-size: 17px"></i></li> </ul> </div>' +
+      '<div class="features">  <i class="fa fa-trash-o click" onClick="onDelete(this)" style="font-size: 24px; display: table"' +
       "></i> </div></div>";
     var li = document.createElement("li");
     li.id = "li_num_" + contador;
     li.priority = contador;
     li.className = "li_num_" + contador;
+    li.text = inputValue;
 
     li.innerHTML = cont;
     document.querySelectorAll(".cont_task_lists > ul")[0].appendChild(li);
@@ -36,28 +37,29 @@ function newElement() {
 }
 
 function onClickUp(arrow) {
-  alert(arrow.parentNode.parentNode.parentNode.parentNode.parentNode.priority);
-
-  /* let newPriority = toUpTask.priority - 1;
-
-  //search for the old task and change it
-  let toDownTask = tasks.find((task) => task.priority == newPriority);
-  toDownTask.priority++;
-  toUpTask.priority = newPriority;
-
-  //swap the tasks
-  tasks[newPriority - 1] = toUpTask;
-  tasks[newPriority] = toDownTask;*/
+  var elem = arrow.parentNode.parentNode.parentNode.parentNode.parentNode;
+  if (elem.closest("li").previousSibling != null) {
+    elem.priority = elem.priority - 1;
+    elem.closest("li").previousSibling.priority = elem.priority + 1;
+    elem.parentNode.insertBefore(elem, elem.closest("li").previousSibling);
+  }
 }
 
-function onClickDown(toDownTask) {
-  let newPriority = toDownTask.priority + 1;
+function onClickDown(arrow) {
+  var elem = arrow.parentNode.parentNode.parentNode.parentNode.parentNode;
+  if (elem.closest("li").nextSibling != null) {
+    elem.priority = elem.priority + 1;
+    elem.closest("li").nextSibling.priority = elem.priority - 1;
+    elem.parentNode.insertBefore(elem.closest("li").nextSibling, elem);
+  }
+}
 
-  //search for the old task and change it
-  let toUpTask = tasks.find((task) => task.priority == newPriority);
-  toUpTask.priority--;
-  toDownTask.priority = newPriority;
-  //swap the tasks
-  tasks[newPriority - 2] = toUpTask;
-  tasks[newPriority - 1] = toDownTask;
+function onDelete(trash) {
+  var elem = trash.parentNode.parentNode.parentNode;
+  elem.parentNode.removeChild(elem);
+}
+
+function onClickEdit(edit) {
+  elem = edit.parentNode.parentNode.parentNode.parentNode.parentNode;
+  // alert(elem.text);
 }
